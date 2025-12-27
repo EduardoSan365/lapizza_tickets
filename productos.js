92ce298729e165647d77841ef732b0469af0c45d
aguchi94 <agudemoreno2@gmail.com>
2025-12-27 13:48:32 -0300

    update 3

---
+++ productos.js
@@ -0,0 +1,26 @@
+const PRODUCTOS = [
+    // PIZZAS
+    { id: 1, nombre: "Cancha", descripcion: "", precio: 8000, categoria: "Pizzas", activo: true },
+    { id: 2, nombre: "Muzza", descripcion: "", precio: 13000, categoria: "Pizzas", activo: true },
+    { id: 3, nombre: "Muzza para freezer", descripcion: "", precio: 10000, categoria: "Pizzas", activo: false },
+    { id: 4, nombre: "Provolone", descripcion: "", precio: 15000, categoria: "Pizzas", activo: true },
+    { id: 5, nombre: "Cebolla", descripcion: "", precio: 15000, categoria: "Pizzas", activo: true },
+    { id: 6, nombre: "Napolitana", descripcion: "", precio: 15000, categoria: "Pizzas", activo: true },
+    { id: 7, nombre: "Margarita", descripcion: "", precio: 15000, categoria: "Pizzas", activo: true },
+    { id: 8, nombre: "Roquefort", descripcion: "", precio: 15000, categoria: "Pizzas", activo: true },
+    { id: 9, nombre: "Choclo", descripcion: "", precio: 15000, categoria: "Pizzas", activo: true },
+    { id: 10, nombre: "Jamón", descripcion: "", precio: 17000, categoria: "Pizzas", activo: true },
+    { id: 11, nombre: "Palmito", descripcion: "", precio: 17000, categoria: "Pizzas", activo: true },
+    { id: 12, nombre: "Champiñón", descripcion: "", precio: 17000, categoria: "Pizzas", activo: true },
+    { id: 13, nombre: "Cuatro quesos", descripcion: "", precio: 17000, categoria: "Pizzas", activo: true },
+    { id: 14, nombre: "Rúcula", descripcion: "", precio: 18000, categoria: "Pizzas", activo: true },
+    
+    // EMPANADAS
+    { id: 15, nombre: "Carne corte a cuchillo", descripcion: "", precio: 2800, categoria: "Empanadas", activo: false },
+    { id: 16, nombre: "Pollo", descripcion: "", precio: 2500, categoria: "Empanadas", activo: true },
+    { id: 17, nombre: "Jamón y Queso", descripcion: "", precio: 2500, categoria: "Empanadas", activo: true },
+    { id: 18, nombre: "Espinaca y Muzza", descripcion: "", precio: 2500, categoria: "Empanadas", activo: true },
+    { id: 19, nombre: "Ches Burguer", descripcion: "", precio: 2500, categoria: "Empanadas", activo: true },
+    { id: 20, nombre: "Quesos y cebolla", descripcion: "", precio: 2500, categoria: "Empanadas", activo: true },
+    { id: 21, nombre: "Barbacoa BBQ", descripcion: "", precio: 2500, categoria: "Empanadas", activo: true }
+];
